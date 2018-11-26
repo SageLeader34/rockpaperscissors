74d8586102c7c05b57a6a83b22440b45f571e207
Ricardo GIbson Jr <gibsonricardo34@gmail.com>
2018-11-26 23:12:25 +0000

    NANI

--- js/script.js
+++ js/script.js
@@ -7,8 +7,64 @@
 
 // DOCUMENT READY FUNCTION BELOW
  $("#shoot").click(function(){
+  
 var userChoice = $('#userChoice').val();
 console.log(userChoice);
 
+var computerChoice="";
+
+var randomNumber= Math.floor(Math.random() * 3);
+
+if(randomNumber === 0) {
+ computerChoice = "rock"; 
+}
+
+else if (randomNumber === 1) {
+ computerChoice = "paper";
+}
+
+else if (randomNumber === 2) {
+ computerChoice = "scissors";
+}
+
+if(userChoice==="rock"){
+ if (computerChoice === "rock") {
+  $("#result").val("DRAW");
+ }
+ 
+ else if (computerChoice === "paper") {
+  $("#result").val("COMPUTER WINS");
+ }
+ 
+ else if (computerChoice === "scissors") {
+  $("#result").val("PLAYER WINS");
+ }
+ 
+}
+
+if (userChoice=== "paper"){
+ if (computerChoice ==="rock"){
+$("#result").val("USER WINS");
+ }
+else if (computerChoice === "paper") {
+ $("#result").val("DRAW");
+}  
+else if (computerChoice === "scissors"){
+ $("#result").val("COMPUTER WINS");
+ }
+}
+
+if (userChoice=== "scissors"){
+ if (computerChoice ==="rock"){
+$("#result").val("COMPUTER WINS");
+ }
+else if (computerChoice === "paper") {
+ $("#result").val("USER WINS");
+}  
+else if (computerChoice === "scissors"){
+ $("#result").val("DRAW");
+}
+ 
+}
      
  })
